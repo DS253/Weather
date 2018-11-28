98cea81b05df2f1af563a509f7d1c780ae375fe9
Daniel Stewart <ds@Daniels-MacBook-Pro.local>
2018-11-28 15:05:27 -0800

    Added methods to retrieve data and update UI

--- Weather/Weather/View Controllers/WeatherViewController.swift
+++ Weather/Weather/View Controllers/WeatherViewController.swift
@@ -33,18 +33,52 @@ class WeatherViewController: UIViewController, CLLocationManagerDelegate, CityUp
         locationManager.startUpdatingLocation()
     }
 
+    func getWeatherData(url: String, parameters : [String : String]) {
+        Alamofire.request(url, method: .get, parameters: parameters).responseJSON {
+            response in
+            if response.result.isSuccess {
+                let weatherJSON : JSON = JSON(response.result.value!)
+                self.updateWeatherData(json: weatherJSON)
+            }
+        }
+    }
+    
+    func updateWeatherData(json : JSON) {
+        if let weatherData = json["main"]["temp"].double {
+            weatherModel.temperature = Int(weatherData - 273.15)
+            weatherModel.city = json["name"].stringValue
+            weatherModel.condition = json["weather"][0]["id"].intValue
+            weatherModel.weatherIconName = weatherModel.updateWeatherImage(condition: weatherModel.condition)
+            updateUIWithWeatherData()
+        }
+        else {
+            cityLabel.text = "Weather Unavailable"
+        }
+    }
+    
+    // Update the UI with the appropriate Weather data
+    func updateUIWithWeatherData() {
+        cityLabel.text = weatherModel.city
+        temperatureLabel.text = "\(weatherModel.temperature)"
+        weatherImage.image = UIImage(named: weatherModel.weatherIconName)
+    }
+    
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         let location = locations[locations.count - 1]
-        locationManager.stopUpdatingLocation()
-        locationManager.delegate = nil
-        
-        let latitude = String(location.coordinate.latitude)
-        let longitude = String(location.coordinate.longitude)
-        
-        let params : [String : String] = ["lat" : latitude, "lon" : longitude, "appid" : APP_ID]
-        
+        if location.horizontalAccuracy > 0 {
+            locationManager.stopUpdatingLocation()
+            locationManager.delegate = nil
+            
+            let latitude = String(location.coordinate.latitude)
+            let longitude = String(location.coordinate.longitude)
+            
+            let params : [String : String] = ["lat" : latitude, "lon" : longitude, "appid" : APP_ID]
+            getWeatherData(url: WEATHER_URL, parameters: params)
+        }
     }
+    
     func updateCity(city: String) {
         let parameter : [String : String] = ["q" : city, "appid" : APP_ID]
+        getWeatherData(url: WEATHER_URL, parameters: parameter)
     }
 }
